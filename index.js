6636ecb2f9346dd472ff2b5bba0989ca484c563a
atechnologyalfred <adjekotaa@gmail.com>
2024-11-06 06:34:38 +0100

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import flash from "connect-flash";
 import session from "express-session";
+import pg from "pg"
 import dotenv from "dotenv";
 import ejs from "ejs";
 const port = process.env.PORT || 3000;
@@ -15,16 +16,44 @@ app.use(session ({
 }));
 app.use(flash());
 
-
+const db =  new pg. Client ({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'users',
+    password: 'alfreda.a',
+    port: 5432,
+});
+db.connect();
 app.get ("/", (req, res) => {
     const msg = req.flash("msg");
     res.render("index.ejs", {msg});
 });
-
-app.post("/", (req, res) =>{
-    req.flash("msg", `Thanks for subscribing, we will get you updated`)
-    res.redirect("/");
+let result;
+db.query("SELECT * FROM credentials", (err, res) => {
+    if(err){
+        console.log("Error loading data", err.stack);
+    } else {
+        result = res.rows;
+        console.log(result);
+        
+    }
 });
+
+
+
+app.post("/", async (req, res) =>{
+    const email = req.body.email;
+    const wallet = req.body.wallet;
+    await db.query("INSERT INTO credentials (email, wallet_address) VALUES ($1, $2)", [email, wallet]);
+    console.log(email, wallet);
+    req.flash("msg", `Thanks for subscribing, we will get you updated`)
+    res.render("result.ejs", {result});
+});
+
+
+
+
+
 
 app.listen(port, ()=> {
     console.log(`Server is running on port${port}`);
